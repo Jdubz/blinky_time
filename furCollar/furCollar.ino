f38cceaef0dcd03b86e28c2f5dbf57f85676284a
Josh Wentworth <contact@joshwentworth.com>
2018-06-29 00:22:43 -0700

    asheems code & changed hat (needs revert)

--- furCollar/furCollar.ino
+++ furCollar/furCollar.ino
@@ -1,13 +1,13 @@
 #include <Adafruit_NeoPixel.h>
 
-int LEDPIN = 2;
-int MICPIN = A0; // 0 - 600
-int BUTTONPIN = 3;
-int NUMLEDS = 106;
-int DELAYTIME = 30;
-int NUMMODES = 5;
+const int LEDPIN = 2;
+const int MICPIN = A0; // 0 - 600
+const int BUTTONPIN = 3;
+const int NUMLEDS = 106;
+const int DELAYTIME = 30;
+const int NUMMODES = 2;
 
-int mode = 0;
+int mode = 1;
 int pressed = 0;
 
 int xPos = 0;
@@ -19,25 +19,38 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMLEDS, LEDPIN, NEO_GRB + NEO_KHZ80
 
 //39, 36, 31
 
+int stars[NUMLEDS];
 int threshhold = 100;
-float gain = 6.0;
-int lvl1;
+float gain = 50.0;
 
-void audioTune() {
-  if (threshhold > 30) {
+float getMicLevel() {
+  int lvl1;
+  unsigned long start = millis();
+  int high = 0;
+  int low = 1024;
+  while (millis() - start < DELAYTIME) {
+    int sample = analogRead(MICPIN);
+    if (sample < low) {
+      low = sample;
+    } else if (sample > high) {
+      high = sample;
+    }
+  }
+  lvl1 = high - low;
+  float micLevel = lvl1 / 1024.0;
+  if (threshhold > 20) {
     threshhold = threshhold - 1;
   }
-  gain = 300.0 / threshhold;
-  lvl1 = analogRead(MICPIN);
   if (lvl1 > threshhold) {
     threshhold = lvl1;
   }
-  lvl1 = lvl1 * gain; 
+  gain = 1024.0 / threshhold;
+  // Serial.println(String(micLevel) + " : " + String(gain));
+  return micLevel * gain;
 }
 
 void render() {
   strip.show();
-  delay(DELAYTIME);
   for (int led = 0; led < NUMLEDS; led++) {
     strip.setPixelColor(led, 0, 0, 0);
   }
@@ -52,47 +65,20 @@ void setup() {
 
 void loop() {
   render();
-  audioTune();
+  float micLvl = getMicLevel();
 
   if (digitalRead(BUTTONPIN) == HIGH && pressed == 0) {
     mode = (mode + 1) % NUMMODES;
     pressed = 1;
     frame = 0;
-    Serial.println(mode);
+    // Serial.println(mode);
   } else if (pressed == 1 && digitalRead(BUTTONPIN) == LOW) {
     pressed = 0;
   }
-  
+
   if (mode == 0) {
-    int rate = 30;
-    xPos = floor(frame/rate);
-    for (int yPos = 0; yPos < Y; yPos++) {
-      if (frame % 30 > 20) {
-        strip.setPixelColor(bucketMap[xPos][yPos], 0, 0, 100);
-      } else if (frame % 30 > 10) {
-        strip.setPixelColor(bucketMap[xPos][yPos], 100, 0, 0);
-      } else {
-        strip.setPixelColor(bucketMap[xPos][yPos], 0, 100, 0);
-      }
-    };
-    frame = (frame + 1) % (X * rate);
+    // do nothing
   } else if (mode == 1) {
-    if (lvl1 > lvl2) {
-      if (lvl1 > 600) {
-        lvl2 = 600;
-      } else {
-        lvl2 = lvl1;
-      }
-    }
-    yPos = int(lvl2/75);
-    for (xPos = 0; xPos < X; xPos++) {
-      strip.setPixelColor(bucketMap[xPos][yPos],125,0,0);
-      for (int blue = yPos - 1; blue >= 0; blue--) {
-        strip.setPixelColor(bucketMap[xPos][blue], 0, 0, 125);
-      }
-    }
-    lvl2 = lvl2 - 30;
-  } else if (mode == 2) {
     int create = random(3);
     int newStars;
     if (create == 0) {
@@ -100,7 +86,7 @@ void loop() {
     } else {
       newStars = 0;
     }
-    int createStars = newStars + ceil(lvl1/10);
+    int createStars = newStars + ceil(micLvl * 20);
     for (int num = 0; num < createStars; num++) {
       stars[random(NUMLEDS)] = 150;
     }
