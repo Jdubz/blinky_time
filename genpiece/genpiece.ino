6c5ac0d249715673e59d40f1e8c0ad8880b9fec5
Gen Bee <gen@gen.gen>
2018-12-16 14:22:38 -0800

    add ice

--- genpiece/genpiece.ino
+++ genpiece/genpiece.ino
@@ -3,34 +3,22 @@
 const int LEDPIN = 2;
 const int LEDS = 9;
 const int BUTTONPIN = 3;
-const int NUMMODES = 2;
+const int NUMMODES = 3;
 
 int frameRate = 30;
 int frame = 0;
 
 
-int mode = 1;
+int mode = 2;
 int pressed = 0;
-
-int levels[9][2] = {
-  {0,0},
-  {1,0},
-  {0,0},
-  {0,0},
-  {1,0},
-  {0,0},
-  {1,0},
-  {0,0},
-  {0,0}
-};
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
-void renderStrip() {
+int renderStrip(int G, int R, int B) {
   strip.show();
   delay(frameRate);
   for (int led = 0; led < LEDS; led++) {
-    strip.setPixelColor(led, 20, 50, 0);
+    strip.setPixelColor(led, G, R, B);
   }
   frame++;
 }
@@ -55,23 +43,35 @@ void setup() {
   delay(1000);
 }
 
-void loop() {
+void buttonCheck() {
   if (digitalRead(BUTTONPIN) == HIGH && pressed == 0) {
     mode = (mode + 1) % NUMMODES;
     pressed = 1;
     if (mode == 0) {
-      renderStrip();
+      renderStrip(0, 0, 0);
     }
   } else if (pressed == 1 && digitalRead(BUTTONPIN) == LOW) {
     pressed = 0;
   }
+}
 
+int levels[9][4] = {
+  {0,0,0,0},
+  {1,0,0,0},
+  {0,0,0,0},
+  {0,0,0,0},
+  {1,0,0,0},
+  {0,0,0,0},
+  {1,0,0,0},
+  {0,0,0,0},
+  {0,0,0,0}
+};
+
+void slowFire() {
+  int base[3] = {20, 50, 0};
   
-  if (mode == 0) {
-    sleep();
-  } else if (mode == 1) {
-    if (frame == 50) {
-      int spark = random(6);
+  if (frame == 50) {
+      int spark = random(LEDS);
       Serial.println(spark);
       levels[spark][0] = 1;
       frame = 0;
@@ -86,11 +86,47 @@ void loop() {
         levels[pixel][1]--;
       }
     }
-  
+
     for (int render = 0; render < LEDS; render++) {
-      strip.setPixelColor(render, levels[render][1] + 20, levels[render][1] + 50, 0);
+      strip.setPixelColor(render, levels[render][1] + base[0], levels[render][1] + base[1], base[2]);
+    }
+    renderStrip(base[0], base[1], base[2]);
+}
+
+void ice() {
+  int base[3] = {30, 10, 40};
+  
+  if (frame == 50) {
+      int spark = random(LEDS);
+      Serial.println(spark);
+      levels[spark][0] = 1;
+      frame = 0;
     }
   
-    renderStrip();
+    for (int pixel = 0; pixel < LEDS; pixel++) {
+      if (levels[pixel][0] == 1 && levels[pixel][1] < 150) {
+        levels[pixel][1]++;
+      } else if (levels[pixel][0] == 1 && levels[pixel][1] == 150) {
+        levels[pixel][0] = 0;
+      } else if (levels[pixel][0] == 0 && levels[pixel][1] > 0) {
+        levels[pixel][1]--;
+      }
+    }
+
+    for (int render = 0; render < LEDS; render++) {
+      strip.setPixelColor(render, levels[render][1]/2 + base[0], levels[render][1]/3 + base[1], levels[render][1] + base[2]);
+    }
+    renderStrip(base[0], base[1], base[2]);
+}
+
+void loop() {
+  buttonCheck();
+  
+  if (mode == 0) {
+    sleep();
+  } else if (mode == 1) {
+    slowFire();
+  } else if (mode == 2) {
+    ice();
   }
 }
