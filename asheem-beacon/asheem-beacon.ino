6ba2a5c1ba4a9a6bcdd9a4cba7b4b1f44d8b5414
Josh Wentworth <contact@joshwentworth.com>
2019-07-24 19:38:40 -0700

    asheem beacon for pole beacon

--- asheem-beacon/asheem-beacon.ino
+++ asheem-beacon/asheem-beacon.ino
@@ -1,12 +1,37 @@
 #include <Adafruit_NeoPixel.h>
 int LEDPIN = 2;
-const int LEDS = 47;
+const int LEDS = 50;
 int frameRate = 30;
 int frame = 0;
+int maxFrames = 300;
 
 int ledLvls[LEDS][2];
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+void phase1() {
+  if (!((frame / frameRate) % 2)) {
+    for (int led = 0; led < LEDS; led++) {
+      strip.setPixelColor(led, 200, 100, 0);
+    }
+  }
+}
+
+void phase2() {
+  strip.setPixelColor(LEDS - ((frame) % LEDS), 0, 200, 100);
+  strip.setPixelColor(LEDS - ((frame+1) % LEDS), 0, 200, 80);
+  strip.setPixelColor(LEDS - ((frame+2) % LEDS), 0, 200, 60);
+  strip.setPixelColor(LEDS - ((frame+3) % LEDS), 0, 200, 40);
+  strip.setPixelColor(LEDS - ((frame+4) % LEDS), 0, 200, 20);
+  strip.setPixelColor(LEDS - ((frame+5) % LEDS), 0, 200, 00);
+}
+
+void nextFrame() {
+  frame++;
+  if (frame == maxFrames) {
+    frame = 0;
+  }
+}
 
 void renderStrip() {
   strip.show();
@@ -28,36 +53,12 @@ void setup() {
 }
 
 void loop() {
-  for (int star = 0; star < LEDS; star++) {
-    if (ledLvls[star][0] > 0) {
-      if (ledLvls[star][1] == 1) {
-        if (ledLvls[star][0] < 100) {
-          ledLvls[star][0] +=5;
-        } else {
-          ledLvls[star][1] = 0;
-        }
-      } else {
-        ledLvls[star][0] -=5;
-      }
-      float starLvl = ledLvls[star][0] / 100.0;
-      strip.setPixelColor(star, 150 * starLvl, 0, 0);
-    }
+  if (frame > (maxFrames / 2)) {
+    phase2();
+  } else {
+    phase1();
   }
-  if (frame == 5) {
-    int newStar = random(LEDS);
-    int newStar2 = random(LEDS);
-    int newStar3 = random(LEDS);
-    int newStar4 = random(LEDS);
-    ledLvls[newStar][0] = 1;
-    ledLvls[newStar][1] = 1;
-    ledLvls[newStar2][0] = 1;
-    ledLvls[newStar2][1] = 1;
-    ledLvls[newStar3][0] = 1;
-    ledLvls[newStar3][1] = 1;
-    ledLvls[newStar4][0] = 1;
-    ledLvls[newStar4][1] = 1;
-    frame = 0;
-  }
-  frame++;
+  
+  nextFrame();
   renderStrip();
 }
