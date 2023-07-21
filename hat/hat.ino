a517850e835fd14ea7a6e7afa6e8af466a7b5e20
Josh Wentworth <contact@joshwentworth.com>
2023-07-20 19:08:59 -0700

    pull key & base blue

--- hat/hat.ino
+++ hat/hat.ino
@@ -2,11 +2,23 @@
 
 #define LED_PIN     2
 #define NUM_LEDS    40
-#define BRIGHTNESS  50
+#define PULL_PIN    5
 
 const int frameRate = 30;
+bool isLow = false;
 
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void pullKey() {
+  int pingFreq = 10;
+  int now = millis();
+  int seconds = now/1000;
+  if (!(seconds % pingFreq) && !isLow) {
+    digitalWrite(PULL_PIN, LOW);
+  } else if ((seconds % pingFreq) && isLow) {
+    digitalWrite(PULL_PIN, HIGH);
+  }
+}
 
 void renderStrip() {
   strip.show();
@@ -41,6 +53,8 @@ void startup() {
 }
 
 void setup() {
+  pinMode(PULL_PIN, OUTPUT);
+
   delay(3000);
   strip.begin();
   startup();
@@ -49,6 +63,7 @@ void setup() {
 int sparks[NUM_LEDS][3];
 
 void loop() {
+  pullKey();
 
   if (random(4) == 1) {
     int sparkId = random(NUM_LEDS);
@@ -72,7 +87,7 @@ void loop() {
         sparks[thisSpark][2] = 0;
       } 
     }
-    strip.setPixelColor(thisSpark, sparks[thisSpark][0], sparks[thisSpark][1], 0);
+    strip.setPixelColor(thisSpark, sparks[thisSpark][0], sparks[thisSpark][1], 50);
   }
   
   renderStrip();
